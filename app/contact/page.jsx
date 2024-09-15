2663ef4b84f095c7362dc103092d1190e047f696
David Hamilton <davidhamilton473@gmail.com>
2024-09-15 18:31:45 -0400

    fixed '

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -60,7 +60,8 @@ const Contact = () => {
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+            <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
+
               <p className="text-white/60">
                 Reach out to book a personal training session or inquire about fitness programs.
               </p>
